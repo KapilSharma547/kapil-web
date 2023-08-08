4bab980250f556d4b2bab51034d4bbb4e96f231a
Anit <kapilsharmanfl97@gmail.com>
2023-08-08 15:00:18 +0530

    about-done

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,20 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import MobileNav from "./Components/MobileNav/MobileNav";
 import Layout from "./Components/Layout/Layout.js";
+import About from "./pages/About/About.js";
+import { useTheme } from "./context/ThemeContext";
 
 const App = () => {
+  const [theme] = useTheme();
   return (
     <>
-      <div>
+      <div id={theme}>
         <ToastContainer />
         <MobileNav />
         <Layout />
+        <div className="container">
+          <About />
+        </div>
       </div>
     </>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "./context/ThemeContext";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
