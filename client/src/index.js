154a1c7784e7c1eaafb724307cef710a89bfd592
kapil <kapilsharmanfl97@gmail.com>
2024-01-18 16:44:14 +0530

    Theme-Context

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./global.css";
+import { ThemeProvider } from "./contaxt/ThemeContex";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useTheme } from "../contaxt/ThemeContex";
 
 const Home = () => {
-  return <div>Home</div>;
+  // const { theme, toggleTheme } = useTheme();
+  // <button onClick={() => toggleTheme()}>handle</button>
+  return (
+    <>
+      <h2>Home</h2>
+    </>
+  );
 };
 
 export default Home;
